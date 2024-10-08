71073a3980aec1eb81cfd24f7cff2fc0f87e863a
Chaitanya Govind <pchaitu2005@gmail.com>
2024-10-08 22:50:32 +0530

    dashboard created

--- src/components/Apifile.jsx
+++ src/components/Apifile.jsx
@@ -1,59 +1,140 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useEffect, useState } from 'react'
-import './Apifile.css'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import '../styles/Apifile.css';
 
 
 function Apifile() {
-    const [search,setSearch]=useState("");const[currency,setCurrency]=useState([])
-useEffect(()=>{
-  axios.get('https://openapiv1.coinstats.app/coins',{headers:{'X-API-KEY':'ezqfxMCr43u8rWI699v8oPVFz4Ao5+/x85p3e4SIQEg='}}).then(res=>setCurrency(res.data.result)).catch(err=>console.log(err)
-  )
-   
-},[])
+  const [search, setSearch] = useState("");
+  const [currency, setCurrency] = useState([]);
+  const [topMovers, setTopMovers] = useState({ gainers: [], losers: [] });
+
+
+
+  
+
+  useEffect(() => {
+    axios.get('https://openapiv1.coinstats.app/coins', {
+      headers: {
+        'X-API-KEY': 'ezqfxMCr43u8rWI699v8oPVFz4Ao5+/x85p3e4SIQEg='
+      }
+    })
+      .then(res => {
+        const sortedData = res.data.result.sort((a, b) => b.priceChange1d - a.priceChange1d);  // Sort by 24h change
+        setCurrency(res.data.result);
+
+        // Extract top gainers and losers
+        const gainers = sortedData.filter(val => val.priceChange1d > 0).slice(0, 5);
+        const losers = sortedData.filter(val => val.priceChange1d < 0).slice(0, 5);
+        setTopMovers({ gainers, losers });
+      })
+      .catch(err => console.log(err));
+  }, []);
+
   return (
-    <div>
-          <h1> Crypto currency</h1>
-       <input type="text" placeholder='search' onChange={(e)=>setSearch(e.target.value)}/>
-       <table>
-        <thead>
-        <tr>
-          <th>Rank</th>
-          <th>Name</th>
-          <th>Symbol</th>
-          <th>Market cap</th>
-          <th>Price</th>
-          <th>Available supply</th>
-          <th>volume(24hr)</th>
-        </tr>
-        </thead>
-        <tbody>
-          {
-            currency.filter((val=>{
-              return val.name.toLowerCase().includes(search.toLocaleLowerCase())
-            })).map((val)=>{
-              return <tr style={{marginLeft:'70px'}} key={val._id}>
-                <td className='rank'>{val.rank}</td>
-                <td style={{display:'flex',gap:'10px'}}>
-                  <a href={val.websiteurl}>
-                    <img  src={val.icon} alt="" style={{ width: '40px', height: '40px' }} />
-                  </a>
-                    <p>{val.name}</p>
-                </td>
-                <td className='symbol'>{val.symbol}</td>
-                <td>
-                  ${val.marketCap}
-                </td>
-                <td>${val.price.toFixed(2)}</td>
-                <td>{val.availableSupply}</td>
-                <td>{val.volume.toFixed(0)}</td>
-              </tr>
-            })
-          }
-        </tbody>
-       </table>
+    <>
+      <Nav className="nav" activeKey="/home">
+      <div className="navbar-left">
+        <Nav.Item>
+          <Nav.Link href="/home" className="n">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1" className="n">About</Nav.Link>
+        </Nav.Item>
+      </div>
+      <div className="navbar-right login-signup">
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">Login</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-3">Sign Up</Nav.Link>
+        </Nav.Item>
+      </div>
+    </Nav>
+    <div className="crypto-container">
+      <h1 className="heading">Crypto Currency Prices</h1>
+      <input
+        type="text"
+        className="search-bar"
+        placeholder="Search by name..."
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      {/* Top Movers Section */}
+      <div className="top-movers">
+      {/* Gainers Section */}
+      <div className="mover gainers">
+        <h2>Top Gainers</h2>
+        <ul>
+          {/* Render items twice for continuous scrolling */}
+          {[...topMovers.gainers, ...topMovers.gainers].map((coin, index) => (
+            <li key={index}>
+              <div className="mover-item">
+                <img src={coin.icon} alt={coin.name} className="mover-icon" />
+                <span>{coin.name}</span>
+                <span className="mover-change">+{coin.priceChange1d.toFixed(2)}%</span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/* Losers Section */}
+      <div className="mover losers">
+        <h2>Top Losers</h2>
+        <ul>
+          {/* Render items twice for continuous scrolling */}
+          {[...topMovers.losers, ...topMovers.losers].map((coin, index) => (
+            <li key={index}>
+              <div className="mover-item">
+                <img src={coin.icon} alt={coin.name} className="mover-icon" />
+                <span>{coin.name}</span>
+                <span className="mover-change red">{coin.priceChange1d.toFixed(2)}%</span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  )
+       <div className="table-wrapper">
+        <table className="crypto-table">
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Name</th>
+              <th>Symbol</th>
+              <th>Market Cap</th>
+              <th>Price</th>
+              <th>Supply</th>
+              <th>Volume (24hr)</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currency.filter((val) => val.name.toLowerCase().includes(search.toLowerCase())).map((val) => (
+              <tr key={val._id}>
+                <td>{val.rank}</td>
+                <td className="currency-name">
+                  <a href={val.websiteurl}>
+                    <img src={val.icon} alt={val.name} className="currency-icon" />
+                  </a>
+                  {val.name}
+                </td>
+                <td>{val.symbol}</td>
+                <td>${val.marketCap.toLocaleString()}</td>
+                <td>${val.price.toFixed(2)}</td>
+                <td>{val.availableSupply.toLocaleString()}</td>
+                <td>{val.volume.toLocaleString()}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+    </>
+  );
 }
 
-export default Apifile
+export default Apifile;
