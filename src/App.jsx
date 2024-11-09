f30d703f821552901eb7e677bb2e783163be6716
Chaitanya Govind <pchaitu2005@gmail.com>
2024-11-09 19:25:25 +0530

    sefs

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,18 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/' element={<Landingpage/>}/>
-      <Route path='/home' element={<Apifile/>}/>
+    <Route path='/' element={<Apifile/>}/>
       <Route path='/register' element={<Register/>}/>
-      <Route path='/predict' element={<Bitprediction/>}/>
+      <Route path='/predict/btc' element={<Bitprediction/>}/>
+      <Route path='/predict/bnb' element={<BnbPrediction/>}/>
+      <Route path='/predict/doge' element={<DogecoinPrediction/>}/>
+      <Route path='/predict/eth' element={<EthereumPrediction/>}/>
+      <Route path='/predict/sol' element={<SolanaPrediction/>}/>
+      <Route path='/predict/usdt' element={<TetherPrediction/>}/>
+      <Route path='/predict/ton' element={<ToncoinPrediction/>}/>
+      <Route path='/predict/trx' element={<TronPrediction/>}/>
+      <Route path='/predict/usdc' element={<UsdcPrediction/>}/>
+      <Route path='/predict/xrp' element={<XRPPrediction/>}/>
     </Routes>
     </BrowserRouter>
   )
