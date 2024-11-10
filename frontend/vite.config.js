da5503b31cc867ec15f0e2630b6f1270563e18c6
KUSUMA RUSHIKESH <141169227+rushi-k12@users.noreply.github.com>
2024-11-10 20:31:42 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,10 +7,10 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-       target: "https://se-project-backend-jfga.onrender.com",
-         changeOrigin: true, 
+       target: "http://localhost:3000",
+        
        secure:false,
-         rewrite: (path) => path.replace(/^\/api/, ''),
+        
       
     }}
   },
