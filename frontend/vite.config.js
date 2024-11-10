3802d15ef054e19654116f83f80a4389ff2a6224
KUSUMA RUSHIKESH <141169227+rushi-k12@users.noreply.github.com>
2024-11-10 20:03:35 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+//http://localhost:3000
 export default defineConfig({
   server:{
     proxy:{
       '/api':{
-       target: "http://localhost:3000",
+       target: "https://se-project-backend-jfga.onrender.com",
        secure:false,
       
     }}
