644c62a5f534c8f7b102f68af028b3f728ad75ad
Chaitanya-sesha-govind <141259100+Chaitanya-sesha-govind@users.noreply.github.com>
2024-11-10 23:25:25 +0530

    Update bitprediction.jsx

--- frontend/src/components/bitprediction.jsx
+++ frontend/src/components/bitprediction.jsx
@@ -11,7 +11,7 @@ function BitPrediction() {
 
   // Fetch historical data (assuming your Flask backend is running on http://127.0.0.1:5000)
   useEffect(() => {
-    axios.get('https://se-project-backend-jfga.onrender.com/api/bitcoin/historical')
+    axios.get('https://se-project-1-i0i9.onrender.com/api/bitcoin/historical')
       .then(response => {
         if (Array.isArray(response.data)) {
           setHistoricalData(response.data);
@@ -30,7 +30,7 @@ function BitPrediction() {
     }
 
     try {
-      const response = await axios.post('https://se-project-backend-jfga.onrender.com/api/bitcoin/predict', { date: predictionDate });
+      const response = await axios.post('https://se-project-1-i0i9.onrender.com/api/bitcoin/predict', { date: predictionDate });
       setPredictedValue(response.data.predicted_value);
       setForecast(response.data.forecast);
      
